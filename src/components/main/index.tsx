f693b2ec8d7638d16583b50265f7a6a5b6b49b4a
hyunoh-modori <hyunoh.bae@modori.io>
2023-08-31 03:40:27 +0900

    fix providers

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -33,21 +33,16 @@ const Main = () => {
   return (
     <>
       <Container>
-        <WebAppProvider
-          options={{
-            smoothButtonsTransition: true,
-          }}>
-          <BackButton onClick={() => console.log('Hello, I am back button!')} />
-          <div>
-            <h1>나의 수익: 0</h1>
-          </div>
-          <ButtonWrapper>
-            <button>Adding AD</button>
-            <button>View AD for airdrops!</button>
-            <button>connect wallet!</button>
-          </ButtonWrapper>
-          <MainButton text="ON CLICK!!" onClick={handleClick} disabled={false}></MainButton>
-        </WebAppProvider>
+        <BackButton onClick={() => console.log('Hello, I am back button!')} />
+        <div>
+          <h1>나의 수익: 0</h1>
+        </div>
+        <ButtonWrapper>
+          <button>Adding AD</button>
+          <button>View AD for airdrops!</button>
+          <button>connect wallet!</button>
+        </ButtonWrapper>
+        <MainButton text="ON CLICK!!" onClick={handleClick} disabled={false}></MainButton>
       </Container>
     </>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Script src="https://telegram.org/js/telegram-web-app.js" />
+    <WebAppProvider
+      options={{
+        smoothButtonsTransition: true,
+      }}>
       <Component {...pageProps} />
-    </>
+    </WebAppProvider>
   );
 }
