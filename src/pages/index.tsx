1148b0aa9f6e20780476ab0a02501e1c60900e22
hyunoh-modori <hyunoh.bae@modori.io>
2023-08-31 03:11:20 +0900

    set layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
-import { Inter } from 'next/font/google';
 import { BackButton, MainButton, WebAppProvider, useShowPopup } from '@vkruglikov/react-telegram-web-app';
 import { styled } from 'styled-components';
 
-const inter = Inter({ subsets: ['latin'] });
-
 const Container = styled.section`
-  min-height: 100%;
+  min-height: 100vh;
+`;
+
+const ButtonWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  margin: 50px auto;
+  > * + * {
+    margin-top: 20px;
+  }
+  button {
+    height: 40px;
+    border: 0;
+    border-radius: 10px;
+    background-color: #007bff;
+    color: #fff;
+  }
 `;
 
 export default function Home() {
@@ -27,9 +41,11 @@ export default function Home() {
           <div>
             <h1>나의 수익: 0</h1>
           </div>
-          <button>Adding AD</button>
-          <button>View AD for airdrops!</button>
-          <button>connect wallet!</button>
+          <ButtonWrapper>
+            <button>Adding AD</button>
+            <button>View AD for airdrops!</button>
+            <button>connect wallet!</button>
+          </ButtonWrapper>
           <MainButton text="ON CLICK!!" onClick={handleClick} disabled={false}></MainButton>
         </WebAppProvider>
       </Container>
