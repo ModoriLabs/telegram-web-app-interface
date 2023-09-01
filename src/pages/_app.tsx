b5444ce1fe01d495098a3846e6c93946af90cfdf
hyunoh-modori <hyunoh.bae@modori.io>
2023-09-01 14:32:21 +0900

    add toast message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
-import Navigation from "@/components/navigation";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import type { AppProps } from "next/app";
-import Script from "next/script";
+import Navigation from '@/components/navigation';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import { GlobalStyle } from "@/styles/globals";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { GlobalStyle } from '@/styles/globals';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   const manifestUrl =
-    "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+    'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 
   return (
     <>
       <TonConnectUIProvider manifestUrl={manifestUrl}>
         <QueryClientProvider client={queryClient}>
+          <ToastContainer />
           <GlobalStyle />
           <Navigation />
           <Component {...pageProps} />
